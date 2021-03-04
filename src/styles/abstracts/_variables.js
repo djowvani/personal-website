3577dfaefce334002f565789ab93606f6c54b50d
Giovani AB <gioanhesini@gmail.com>
2021-03-04 19:22:35 -0300

    A variables file for some quick import of colors and z-indexes saves u

---
+++ src/styles/abstracts/_variables.js
@@ -0,0 +1,6 @@
+export const black_bg = '#1D1F1E';
+export const black_bg2 = '#0C0C0C';
+export const verdolas = '#4BFFA5';
+export const verdolas2 = '#0AB178';
+export const cinzin = '#373B40';
+export const cinzin2 = '#585D65';
