d53e1697fb217f87bc44c00ccbb42f2d33d46e88
Giovani AB <gioanhesini@gmail.com>
2021-03-04 17:35:30 -0300

    The web page estructure, with some "notes/sketches"

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,57 @@
+import GlobalStyle from '@/styles/global';
+
+import Head from '@/components/Head/Head'
+import Header from '@/components/Header/Header'
+import Billboard from '@/components/Billboard/Billboard'
+import RightFrame from '@/components/RightFrame/RightFrame'
+import ScrolldownIndicator from '@/components/ScrolldownIndicator/ScrolldownIndicator'
+
 const Home = () => {
-    return <div>Dale</div>
+    // seta de scroll full top
+    // custom cursor
+    // sandwich menu button
+    // timeline
+    // animacao nos links do header
+
+    return (
+        <>
+            <Head
+                title={'Giovani Anhesini | Web Developer'}
+            />
+
+            <GlobalStyle />
+
+            <Header>
+                <span>about me</span>
+                <span>work</span>
+                <span>contact</span>
+                {/* <span>profile</span>
+                <span>experiences</span>
+                <span>abilities</span>
+                <span>projects</span> */}
+            </Header>
+
+            <Billboard>
+                {/* <LeftFrame /> */}
+                <RightFrame
+                    name='Djow.dev_'
+                    occupation='The world´s #02nd strongest Weeb developer!'
+                />
+            </Billboard>
+            <ScrolldownIndicator />
+            <Billboard>
+                {/* <AboutMe /> */}
+            </Billboard>
+            <Billboard>
+                {/* <Work /> */}
+            </Billboard>
+            <Billboard>
+                {/* <Contact /> */}
+            </Billboard>
+
+            {/* <Footer /> */}
+        </>
+    );
 }
 
 export default Home;
